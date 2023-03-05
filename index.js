0467756587e850b3670aabdf9e09d529cfb205a2
arnavbansal25 <arnavbansal.8232@gmail.com>
2023-03-06 00:28:27 +0530

    using JSX instead of createElement

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { key: "1" }, "heading 1");
-const heading2 = React.createElement("h2", { key: "2" }, "heading 2");
+const heading1 = <h1 key="1">heading 1</h1>;
+const heading2 = <h2 key="2">heading 2</h2>;
 
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+const container = (
+  <div id="container">
+    {heading1} {heading2}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
