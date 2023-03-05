be2d7f4ba3ca7e220a2191c0f3e2af744eadba08
arnavbansal25 <arnavbansal.8232@gmail.com>
2023-03-06 01:13:54 +0530

    functional components

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const heading1 = <h1 key="1">heading 1</h1>;
-const heading2 = <h2 key="2">heading 2</h2>;
 
-const container = (
+const H2 = () => <h2 key="2">heading 2</h2>;
+
+const Container = () => (
   <div id="container">
-    {heading1} {heading2}
+    {heading1}
+    <H2 />
+    <H2></H2>
+    {H2()}
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<Container />);
